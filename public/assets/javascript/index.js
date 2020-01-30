6eadd9f6767294337f4efe800bfc766586860edc
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-01-30 15:36:29 -0800

    projects view and index views are near complete

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -10,19 +10,7 @@ const sendMailAddress = "caitlanjeffrey@gmail.com"
 
 var str = "mailto:" + sendMailAddress + "?subject=" + subject + "&body=" + message;
 
-// document.getElementById('send-message-btn').href = str;
-
 $("#send-message-btn").on("click", function(){
     event.preventDefault();
     window.location = str
 })
-
-// $(function () {
-//     $('#emailLink').on('click', function (event) {
-//         alert("Huh");
-//         var email = 'test@theearth.com';
-//         var subject = 'Circle Around';
-//         var emailBody = 'Some blah';
-//         window.location = 'mailto:' + email + '?subject=' + subject + '&body=' + emailBody;
-//     });
-// });
