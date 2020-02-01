48726debd06fcf54f7c0aa419653bc623f4cbf0a
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-02-01 11:07:02 -0800

    saving changes to the server process to remove db

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
-var db = require("./models") 
+// var db = require("./models") 
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.engine(
@@ -40,14 +40,19 @@ require("./routes/apiRoutes")(app);
 var syncOptions = { force: false };
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-    app.listen(PORT, function () {
-        console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
+// db.sequelize.sync(syncOptions).then(function () {
+//     app.listen(PORT, function () {
+//         console.log(
+//             "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//             PORT,
+//             PORT
+//         );
+//     });
+// });
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
 module.exports = app; 
